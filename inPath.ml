8cbc981ace6cec098bcd44636f1cac2b509fd418
rudenoise <rudenoise@gmail.com>
2016-02-19 17:02:41 +0000

    begin reading dir children

--- inPath.ml
+++ inPath.ml
@@ -1,5 +1,10 @@
 open Core.Std
 
+let read_dir path =
+  let children = Sys.readdir path
+  in
+  Array.iter ~f:print_endline children
+
 let spec =
   let open Command.Spec in
   empty
@@ -10,7 +15,7 @@ let command =
     ~summary: "inPath displays all paths beneath the current directory"
     ~readme: (fun () -> "More detailed info")
     spec
-    (fun path () -> printf "%s\n" path)
+    (fun path () -> read_dir path)
 
 let () =
   Command.run ~version:"0.1" ~build_info:"RWO" command
