a09ebb2f30ef364687ff5147a1b4dd6afe700a5a
rudenoise <rudenoise@gmail.com>
2016-05-31 16:22:21 +0100

    remove core.std from path module

--- path.ml
+++ path.ml
@@ -1,25 +1,24 @@
-open Core.Std
-
 let rec read_dir ~path ~paths_list =
-  let children = Sys.readdir path
-  in
-  Array.fold
-    ~init: paths_list
-    ~f:(fun p_list child ->
+  let children = Sys.readdir path in
+  Array.fold_left
+    (
+      fun p_list child ->
         let full_path = match path with
           "./" -> path ^ child
           | _ -> path ^ "/" ^ child
         in
         match (Sys.is_directory full_path) with
-          `Yes -> (full_path ^ "/") :: (read_dir ~path:full_path ~paths_list:p_list)
-          | _ -> full_path :: p_list
-      )
+        | true -> (full_path ^ "/") :: (read_dir ~path:full_path ~paths_list:p_list)
+        | _ -> full_path :: p_list
+    )
+    paths_list
     children
 
 
 let print_path_list ~include_re ~exclude_re ~paths_list =
   List.iter
-    ~f: (fun line ->
+    (
+      fun line ->
         if Str.string_match (Str.regexp include_re) line 0 then begin
           if (Str.string_match (Str.regexp exclude_re) line 0) = false then
             print_endline (Str.replace_first (Str.regexp "\\/\\/") "/" line)
